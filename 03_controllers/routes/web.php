b85305c8f8d3c2f3c6c418ce35ef638cf1b6eba0
najmul-islam <najmulislam519@gmail.com>
2024-07-18 11:57:42 +0600

    delete old folder

--- 03_controllers/routes/web.php
+++ 03_controllers/routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\AdminProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get("/about", [AboutController::class, "show"]);
-// Route::get("/about/{name}", [AboutController::class, "show"]);
-// Route::get("/admin/profile", [AdminProfileController::class, "show"]);
-
-Route::get("/about", [AboutController::class, "show"]);
--- 06_component_and_class_component/resources/views/components/card.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h2>Card Title</h2>
-    <h4>Card Subtitle</h4>
-    <p>Card Description</p>
-</div>
