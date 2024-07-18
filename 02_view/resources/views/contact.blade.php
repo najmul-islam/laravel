f1b8840db6e74d33ae10d7ed55dffb8032e1f123
najmul-islam <najmulislam519@gmail.com>
2024-07-18 11:54:41 +0600

    done view tutorial

--- 02_view/resources/views/contact.blade.php
+++ 02_view/resources/views/contact.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Contact Page</h1>
-    <h3>Hello, {{$name}}. </h3>
+    <h1>Contact me page</h1>
+    <h2>Hello, {{ $name }}</h2>
+    <h3>Roll: {{ $roll }}</h3>
 </body>
+
 </html>
--- 02_view/routes/web.php
+++ 02_view/routes/web.php
@@ -2,29 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get("/about", function () {
-//     return view("about");
-// });
+Route::get("/about", function () {
+    return view("about");
+});
 
-Route::view("/about", "about");
+Route::get("/user/{userId}", function ($userId) {
+    return view("user.profile", ['userId' => $userId]);
+});
+
+// Route::view("/about", "about");
+
 Route::get("/admin/profile", function () {
     return view("admin.profile");
 });
 
+// Route::view("/admin/profile", "admin.profile");
+
 // Route::get("/contact", function () {
-//     return view("contact", ["name" => "Rahul"]);
+//     return view("contact", ["name" => "Sonam"]);
 // });
 
 // Route::view("/contact", "contact", ["name" => "Sonam"]);
 
 // Route::get("/contact", function () {
-//     return view("contact", ["name" => "Sonam", "roll" => 101]);
+//     return view("contact", ["name" => "Sonam", "roll" => 123]);
 // });
 
 Route::get("/contact", function () {
-    return view("contact")->with("name", "Sonam");
+    return view("contact")->with(["name" => "Sonam", "roll" => 1234]);
 });
