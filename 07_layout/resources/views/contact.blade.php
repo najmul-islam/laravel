7de3df389d37fc6503f4e8ca61e15b34f610bd0f
najmul-islam <najmulislam519@gmail.com>
2024-07-19 20:19:49 +0600

    layout using component done

---
+++ 07_layout/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot name="title">Contact</x-slot>
+    <h1>Contact page</h1>
+</x-layout>
---
+++ 07_layout/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot name="title">Home</x-slot>
+    <h1>Home Page</h1>
+</x-layout>
