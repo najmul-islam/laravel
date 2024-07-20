ad2230c491ea22bac41d0ade3d6c7dc8f58b0b3d
najmul-islam <najmulislam519@gmail.com>
2024-07-20 19:29:15 +0600

    layout using template inheritance

--- 07_layout/resources/views/contact.blade.php
+++ 07_layout/resources/views/contact.blade.php
@@ -1,4 +1,16 @@
-<x-layout>
-    <x-slot name="title">Contact</x-slot>
+@extends('layout.adminlayout')
+@section('title', 'Contact')
+
+@section('page-name')
     <h1>Contact page</h1>
-</x-layout>
+    <hr>
+@endsection
+
+@section('banner')
+    <h2>This is contact page banner</h2>
+@endsection
+
+@section('main')
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem pariatur quasi reprehenderit cum
+        consequuntur laborum velit sapiente explicabo quis voluptas?</p>
+@endsection
--- 07_layout/resources/views/home.blade.php
+++ 07_layout/resources/views/home.blade.php
@@ -1,4 +1,20 @@
-<x-layout>
-    <x-slot name="title">Home</x-slot>
-    <h1>Home Page</h1>
-</x-layout>
+@extends('layout.adminlayout')
+@section('title', 'Home')
+
+@section('page-name')
+    <h1>Home page</h1>
+    <hr>
+@endsection
+
+@section('banner')
+    <h2>This is home page banner</h2>
+@endsection
+
+@section('main')
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, nostrum!
+@endsection
+
+@section('default-content')
+    @parent
+    <h2>This is home page text</h2>
+@endsection
