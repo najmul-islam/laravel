6c613be7a4d1a95ec5246e1bb47c418d34192a82
najmul-islam <najmulislam519@gmail.com>
2024-08-03 00:34:37 +0600

    layout - practice again

---
+++ 07_v2_layout/resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout.public-layout>
+    <x-slot name="title">Home Page</x-slot>
+    <x-slot name="banner">
+        <h1>This is home banner</h1>
+        <hr>
+    </x-slot>
+</x-layout.public-layout>
